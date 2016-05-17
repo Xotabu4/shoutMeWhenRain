123111c4ccd9cd7ae962119ecbedc8ce63c46b2a
Alexander Khotemskoy <Xotabu4@gmail.com>
2016-05-17 23:28:05 +0300

    Added simple weather response for now
    - Included owm - library for openweathermap API
    - Rewritten to use decorators
    - Custom keyboard added
    - Simple response for current weather

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from urllib import request, response, parse
 import json
+
+import collections
+
 import mocks
 import config
+import pyowm
+import logging
+
 import telebot
+from telebot import types
 
 __doc__ = '''
 Basic experiments with weather api, and telegram bot library.
@@ -10,30 +17,54 @@ Basic experiments with weather api, and telegram bot library.
 API help and docs:
 
 Telegram = https://www.gitbook.com/book/kondra007/telegram-bot-lessons/details
-Weather = http://openweathermap.org/forecast5
+WeatherAPI = http://openweathermap.org/forecast5
+WeatherLIBRARY = https://github.com/csparpa/pyowm/wiki/Usage-examples
 
 Config.py contains apikeys, not included in project.
 '''
 
-weather_params = {
-    'q': 'Kiev,ua',
-    'mode': 'json',
-    'appid': config.OPENWEATHER['appid']
-}
+owm = pyowm.OWM(config.OPENWEATHER['appid'], language='ru')
 
-encoded_params = parse.urlencode(weather_params)
+logger = telebot.logger
+telebot.logger.setLevel(logging.DEBUG)
+BOT = telebot.TeleBot(config.TELEGRAM['token'])
 
-with request.urlopen('http://api.openweathermap.org/data/2.5/forecast?' + encoded_params) as resp:
-    data = resp.read().decode('utf-8')
-    parsed_json = json.loads(data)
-    print(parsed_json)
 
-def listener(messages):
-    for m in messages:
-        if m.content_type == 'text':
-            bot.send_message(m.chat.id, m.text)
+@BOT.message_handler(commands=['start'])
+def echo_msg(message):
+    BOT.reply_to(message, 'Начинаем!')
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    itembtn1 = types.KeyboardButton('Сейчас')
+    itembtn2 = types.KeyboardButton('Завтра')
+    markup.add(itembtn1, itembtn2)
+    BOT.send_message(chat_id=message.chat.id, text="На когда погоду?", reply_markup=markup)
+
+
+@BOT.message_handler(regexp="Сейчас")
+def now(message):
+    # Hide keyboard, if visible
+    markup = types.ReplyKeyboardHide(selective=False)
+    BOT.send_message(chat_id=message.chat.id, text='Ломлюсь за данными...', reply_markup=markup)
+
+    w = owm.weather_at_place('Kiev,ua').get_weather()
+    logger.info('Получил - ', w.to_JSON())
+    resp = collections.OrderedDict({
+        "Как вообще?": w.get_status(),
+        'Температура:': w.get_temperature(unit='celsius')['temp'],
+        'Дождь?': 'Льет!' if w.get_rain() else 'Неа'
+    })
+
+    for key, value in resp.items():
+        BOT.send_message(chat_id=message.chat.id, text='{0} {1}'.format(key, value))
+
+@BOT.message_handler(regexp="Завтра")
+def tomorrow(message):
+    # Hide keyboard, if visible
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    itembtn1 = types.KeyboardButton('Сейчас')
+    markup.add(itembtn1)
+    BOT.send_message(chat_id=message.chat.id, text='Погоды нет. Еще не реализовано', reply_markup=markup)
+
 
 if __name__ == '__main__':
-     bot = telebot.TeleBot(config.TELEGRAM['token'])
-     bot.set_update_listener(listener)
-     bot.polling(none_stop=True)
+    BOT.polling(none_stop=True)
